75e2a0f2798784f472b005ddcb55ea0b4083c22d
Inna_Features <features.inna@mail.ru>
2018-01-25 17:31:51 +0300

    add owner in model

--- games/models.py
+++ games/models.py
@@ -13,6 +13,7 @@ class GameCategory(models.Model):
 
 
 class Game(models.Model):
+	owner = models.ForeignKey('auth.User', related_name='games', on_delete=models.CASCADE)
 	created = models.DateTimeField(auto_now_add=True)
 	name = models.CharField(max_length=200, unique=True)
 	release_date = models.DateTimeField()
