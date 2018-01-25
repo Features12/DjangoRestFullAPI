2a2468cc89be6d87cff2a0eae8a81e2041c206f7
Inna_Features <features.inna@mail.ru>
2018-01-25 16:38:08 +0300

    add unique name fields

--- games/models.py
+++ games/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class GameCategory(models.Model):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, unique=True)
 
 
 	class Meta:
@@ -14,7 +14,7 @@ class GameCategory(models.Model):
 
 class Game(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, unique=True)
 	release_date = models.DateTimeField()
 	game_category = models.ForeignKey(GameCategory, related_name='games', on_delete=models.CASCADE)
 	played = models.BooleanField(default=False)
@@ -38,7 +38,7 @@ class Player(models.Model):
 		(FEMALE, 'Female'),
 	)
 
-	name = models.CharField(max_length=50, blank=False, default='')
+	name = models.CharField(max_length=50, blank=False, default='', unique=True)
 	created = models.DateTimeField(auto_now_add=True)
 	gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default=MALE)
 
